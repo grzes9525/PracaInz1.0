47b0c8bb8400276f9f786b41eeab5d9038e2d297
Grzesiek <jakubiak-grzegorz@wp.pl>
2017-10-30 20:02:42 +0100

    Commit z starym welcome

---
+++ app/src/main/java/com/pracainz10/WelcomeDB.java
@@ -0,0 +1,11 @@
+package com.pracainz10;
+
+/**
+ * Created by Grzechu on 24.10.2017.
+ */
+
+public class WelcomeDB {
+    static String[] titles = {"Obecna waga", "Waga docelowa", "Cel tygodniowy", "Poziom aktywności", "Nowy dpcelowy dzienny bilans kalorii" };
+    static String[] values = {"68kg", "63kg", "0.5 kg na tydz","umiarkowany", "1245"};
+    static Integer[] id_ = {0, 1, 2, 3,4};
+}
