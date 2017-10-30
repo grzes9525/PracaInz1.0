b2db53594221b9f90493538de4eda09a000fdd4e
Grzesiek <jakubiak-grzegorz@wp.pl>
2017-10-30 20:01:08 +0100

    Commit z starym welcome

--- app/src/main/java/com/pracainz10/WelcomeActivity.java
+++ app/src/main/java/com/pracainz10/WelcomeActivity.java
@@ -1,29 +1,125 @@
 package com.pracainz10;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class WelcomeActivity extends AppCompatActivity {
+
+    private static RecyclerView.Adapter adapter;
+    private RecyclerView.LayoutManager layoutManager;
+    private static RecyclerView recyclerView;
+    private static ArrayList<WelcomeData> data;
+    static View.OnClickListener myOnClickListener;
+    private static ArrayList<Integer> removedItems;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+        myOnClickListener = new MyOnClickListener(this);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+
+        recyclerView = (RecyclerView) findViewById(R.id.my_recycler_view_welcome);
+        recyclerView.setHasFixedSize(true);
+
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+
+        data = new ArrayList<WelcomeData>();
+        for (int i = 0; i < WelcomeDB.titles.length; i++) {
+            data.add(new WelcomeData(
+                    WelcomeDB.titles[i],
+                    WelcomeDB.values[i],
+                    WelcomeDB.id_[i]
+
+            ));
+        }
+
+        removedItems = new ArrayList<Integer>();
+        adapter = new WelcomeAdapter(data);
+        recyclerView.setAdapter(adapter);
     }
+    private static class MyOnClickListener implements View.OnClickListener {
+
+        private final Context context;
+
+        private MyOnClickListener(Context context) {
+            this.context = context;
+        }
+
+        @Override
+        public void onClick(View v) {
+            removeItem(v);
+        }
+
+        private void removeItem(View v) {
+            int selectedItemPosition = recyclerView.getChildPosition(v);
+            RecyclerView.ViewHolder viewHolder
+                    = recyclerView.findViewHolderForPosition(selectedItemPosition);
+            TextView textViewName
+                    = (TextView) viewHolder.itemView.findViewById(R.id.textViewTitle);
+            String selectedName = (String) textViewName.getText();
+            int selectedItemId = -1;
+            for (int i = 0; i < WelcomeDB.titles.length; i++) {
+                if (selectedName.equals(WelcomeDB.titles[i])) {
+                    selectedItemId = WelcomeDB.id_[i];
+                }
+            }
+            removedItems.add(selectedItemId);
+            data.remove(selectedItemPosition);
+            adapter.notifyItemRemoved(selectedItemPosition);
+        }
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        super.onOptionsItemSelected(item);
+        if (item.getItemId() == R.id.action_settings) {
+            //check if any items to add
+            if (removedItems.size() != 0) {
+                addRemovedItemToList();
+            } else {
+                Toast.makeText(this, "Nothing to add", Toast.LENGTH_SHORT).show();
+            }
+        }
+        return true;
+    }
+
+    private void addRemovedItemToList() {
+        int addItemAtListPosition = 3;
+        data.add(addItemAtListPosition, new WelcomeData(
+                WelcomeDB.titles[removedItems.get(0)],
+                WelcomeDB.titles[removedItems.get(0)],
+                WelcomeDB.id_[removedItems.get(0)]
+
+        ));
+        adapter.notifyItemInserted(addItemAtListPosition);
+        removedItems.remove(0);
+    }
+
 
 }
